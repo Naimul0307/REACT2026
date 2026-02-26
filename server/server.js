7c1f34ec4f0ab7d98c23593446e10e40f4030148
unknown <mdnaimul15902@gmail.com>
2026-02-26 22:56:57 +0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const router = require('./router/auth-router')
+
+app.use(express.json());
+
+app.use("/api/auth", router);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
